e61a0c7c3e07a6152ce7304ba92634001f52ae49
dainabeblog <dainabeblog@gmail.com>
2021-07-17 21:15:38 +0900

    アカウント登録、MySQL接続

---
+++ EggData_dev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EggData_dev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
